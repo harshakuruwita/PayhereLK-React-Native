0a334f92ef5d8abd96e2e40469a3d781a502d9c9
Harsha Harischandra <harsha@virtualcapital.co.nz>
2020-03-03 00:23:11 +0530

    init sample code

---
+++ App.js
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  NativeModules,
+  View,
+  Button,
+} from 'react-native';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Payment details</Text>
+        <TextInput
+          style={{margin: 10}}
+          multiline={true}
+          placeholder="Type text to display in toast"
+          onChangeText={text => this.setState({text})}
+        />
+        <Button
+          onPress={() => {
+            NativeModules.Payhere.payOnece();
+          }}
+          title="Call Payhere"
+          color="#841584"
+          accessibilityLabel="Initiate a Payment Request to PayHere Payment Gateway "
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
---
+++ android/app/src/main/java/com/xcapp/payhere/Payhere.java
@@ -0,0 +1,84 @@
+package com.xcapp.payhere;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import lk.payhere.androidsdk.PHConfigs;
+import lk.payhere.androidsdk.PHConstants;
+import lk.payhere.androidsdk.PHMainActivity;
+import lk.payhere.androidsdk.model.InitRequest;
+import lk.payhere.androidsdk.model.Item;
+
+
+public class Payhere extends ReactContextBaseJavaModule {
+
+
+    private final ReactApplicationContext reactContext;
+    private final static int PAYHERE_REQUEST = 11010;
+    public Payhere(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+
+    @Override
+    public String getName() {
+
+        return "Payhere";
+    }
+
+    @ReactMethod
+    public void getStatus(
+
+
+            Callback successCallback) {
+        successCallback.invoke(null, true);
+
+    }
+
+    @ReactMethod
+    public void payOnece() {
+ Log.d("Payment call","Caling payment");
+        InitRequest req = new InitRequest();
+        req.setMerchantId("1210XXX");       // Your Merchant PayHere ID
+        req.setMerchantSecret("ugknesnvn"); // Your Merchant secret (Add your app at Settings > Domains & Credentials, to get this))
+        req.setCurrency("LKR");             // Currency code LKR/USD/GBP/EUR/AUD
+        req.setAmount(1000.00);             // Final Amount to be charged
+        req.setOrderId("230000123");        // Unique Reference ID
+        req.setItemsDescription("Door bell wireless");  // Item description title
+        req.setCustom1("This is the custom message 1");
+        req.setCustom2("This is the custom message 2");
+        req.getCustomer().setFirstName("Saman");
+        req.getCustomer().setLastName("Perera");
+        req.getCustomer().setEmail("samanp@gmail.com");
+        req.getCustomer().setPhone("+94771234567");
+        req.getCustomer().getAddress().setAddress("No.1, Galle Road");
+        req.getCustomer().getAddress().setCity("Colombo");
+        req.getCustomer().getAddress().setCountry("Sri Lanka");
+
+//Optional Params
+        req.getCustomer().getDeliveryAddress().setAddress("No.2, Kandy Road");
+        req.getCustomer().getDeliveryAddress().setCity("Kadawatha");
+        req.getCustomer().getDeliveryAddress().setCountry("Sri Lanka");
+        req.getItems().add(new Item(null, "Door bell wireless", 1, 1000.0));
+
+
+        Intent intent = new Intent(reactContext, PHMainActivity.class);
+        intent.putExtra(PHConstants.INTENT_EXTRA_DATA, req);
+        PHConfigs.setBaseUrl(PHConfigs.SANDBOX_URL);
+
+        reactContext.startActivityForResult(intent, PAYHERE_REQUEST, Bundle.EMPTY);
+
+       // successCallback.invoke(intent);
+    }
+}
